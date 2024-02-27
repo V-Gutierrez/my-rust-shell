a7328f729e6f171fd1c93406dc23c9cc1cdabc20
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2024-02-27 12:27:09 -0300

    refactor: improve prompt and handle command execution errors

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
-use std::{io::{stdin, stdout, Write}, process::Command};
+use std::{
+    io::{stdin, stdout, Write},
+    process::Command,
+};
 
 #[allow(unused_must_use)]
 fn main() {
     loop {
-    // TODO: Improve Prompt
-    print!(">>> ");
+        // TODO: Improve Prompt
+        print!("--->>> ");
 
-    stdout().flush();
+        stdout().flush();
 
-    let mut input = String::new();
+        let mut input = String::new();
 
-    stdin()
-        .read_line(&mut input)
-        .unwrap();
+        stdin().read_line(&mut input).unwrap();
 
-    // Remove the newline character from the input
-    let command = input.trim();
-    
-    let mut child = Command::new(command).spawn().unwrap();
+        // Remove the newline character from the input
+        let command = input.trim();
 
-    child.wait();
-}}
+        #[allow(unused_mut)]
+        let mut child = Command::new(command).spawn();
+
+        match child {
+            Ok(mut child) => {
+                child.wait();
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
+}
