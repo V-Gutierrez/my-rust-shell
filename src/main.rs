b1c022444fa7a5a0ed94bb7accde5a72e155ea64
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2024-02-27 14:26:02 -0300

    feat: add cd command support

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::{
+    env::{self},
     io::{stdin, stdout, Write},
+    path::Path,
     process::Command,
 };
 
@@ -22,15 +24,27 @@ fn main() {
         // Use the rest of the iteration (the same variable user_input, this is Iterators behavior) as the command arguments (e.g. "-l" or "hello")
         let command_args = user_input;
 
-        #[allow(unused_mut)]
-        let mut child = Command::new(command).args(command_args).spawn();
+        match command {
+            // cd command must be a built in (https://unix.stackexchange.com/a/38809)
+            "cd" => {
+                // default to '/' as new directory if one was not provided
+                let new_dir = command_args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
 
-        match child {
-            Ok(mut child) => {
-                child.wait();
+                if let Err(e) = env::set_current_dir(root) {
+                    eprintln!("{}", e);
+                }
             }
-            Err(e) => {
-                println!("Error: {}", e);
+            command => {
+                #[allow(unused_mut)]
+                let mut child = Command::new(command).args(command_args).spawn();
+
+                match child {
+                    Ok(mut child) => {
+                        child.wait();
+                    }
+                    Err(e) => eprintln!("{}", e),
+                };
             }
         }
     }
