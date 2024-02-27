8c562eec3f5805e543fbf2039233bcab9b9d2f23
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2024-02-27 12:46:53 -0300

    feat: add arguments parsing

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,15 @@ fn main() {
 
         stdin().read_line(&mut input).unwrap();
 
-        // Remove the newline character from the input
-        let command = input.trim();
+        // Remove the newline character from the input and split it into an iterator
+        let mut user_input = input.trim().split_whitespace();
+        // Use the next iteration (e.g. the first word) as the command (e.g. "ls" or "echo")
+        let command = user_input.next().unwrap();
+        // Use the rest of the iteration (the same variable user_input, this is Iterators behavior) as the command arguments (e.g. "-l" or "hello")
+        let command_args = user_input;
 
         #[allow(unused_mut)]
-        let mut child = Command::new(command).spawn();
+        let mut child = Command::new(command).args(command_args).spawn();
 
         match child {
             Ok(mut child) => {
